f1d8ae70311f57b90a2a0c7ec48fe64bb50fa779
Alex Doss <alex.doss42@gmail.com>
2020-06-07 00:03:57 -0600

    adds server connection and listens

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const EventEmitter = require("events");
 var Logger = require("./logger");
+const http = require("http");
+const server = http.createServer();
+
+server.on('connection', (socket) => {
+  console.log("New Connection...");
+});
 
 logger.on("messageLogged", (arg) => {
   console.log('Listener called', arg);
@@ -7,3 +13,5 @@ logger.on("messageLogged", (arg) => {
 
 const logger = new Logger();
 logger.log('message');
+
+server.listen(3000);
