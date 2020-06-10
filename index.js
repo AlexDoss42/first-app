98012c85f1dcce86dd159ee1a7e362bffd904b6e
Alex Doss <alex.doss42@gmail.com>
2020-06-09 21:14:30 -0600

    adds courses route

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
+app.get('/api/courses', (req, res) => {
+  res.send([1,2[3]]);
+});
+
 app.listen(3000, () => console.log("It's over anakin. I have the high port"));
