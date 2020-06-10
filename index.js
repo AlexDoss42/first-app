3dd39ad27e18fbec12c43f6e341aa516b44db703
Alex Doss <alex.doss42@gmail.com>
2020-06-09 21:25:44 -0600

    adds post route for demo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses =[
   {id: 1, name: 'course1'},
   {id: 2, name: 'course2'},
@@ -22,6 +24,15 @@ app.get('/api/courses/:id', (req, res) => {
   }
   res.send(course);
 });
+
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`It's over anakin. I have the ${port} port`));
