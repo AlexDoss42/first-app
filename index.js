20f87d0f5d59aad57b65b97068e14d315d1a4d72
Alex Doss <alex.doss42@gmail.com>
2020-06-09 21:23:12 -0600

    gives some real logic to the get course route

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const courses =[
+  {id: 1, name: 'course1'},
+  {id: 2, name: 'course2'},
+  {id: 3, name: 'course3'}
+]
+
 app.get('/', function(req, res){
   res.send('Hello World');
 });
@@ -10,7 +16,11 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.get('/api/courses/:id', (req, res) => {
-  res.send(req.params.id);
+  let course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("the course with that id was not found")
+  }
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
