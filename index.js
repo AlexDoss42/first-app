cb3ca9135d7ddf1fb4618d4b8ce94ccc0211956c
Alex Doss <alex.doss42@gmail.com>
2020-06-11 14:52:06 -0600

    adds some built in middlewares for demo

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
 app.use(function(req, res, next){
   console.log("logging...");
   next();
