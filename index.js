b808b3e39edc14670f37a3286b2da301f50d7f7e
Alex Doss <alex.doss42@gmail.com>
2020-06-11 14:54:29 -0600

    adds morgan and helmet

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+
 app.use(function(req, res, next){
   console.log("logging...");
   next();
