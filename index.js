1e72196800fa2c826981b6c96ffc92d903cfb589
Alex Doss <alex.doss42@gmail.com>
2020-06-11 14:59:54 -0600

    shows env

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const morgan = require('morgan');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+app.get('env');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
