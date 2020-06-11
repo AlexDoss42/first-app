02acc8950e55fcbe1a7c15afa5bdd38b9e34939f
Alex Doss <alex.doss42@gmail.com>
2020-06-11 14:47:25 -0600

    builds a middleware to demo

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use(function(req, res, next){
+  console.log("logging...");
+  next();
+})
+
 const courses =[
   {id: 1, name: 'course1'},
   {id: 2, name: 'course2'},
