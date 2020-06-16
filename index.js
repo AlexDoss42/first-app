728980b79175861fdd9b45b058a5a64e1768b4f6
Alex Doss <alex.doss42@gmail.com>
2020-06-15 21:24:17 -0600

    refactors a little more with courses

--- index.js
+++ index.js
@@ -11,17 +11,11 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-app.get('env');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'));
 app.use(helmet());
 app.use('/api/courses', courses);
-
-console.log('Application Name: ' + config.get('name'));
-console.log('Mail Sever: ' + config.get('mail.host'))
 
 if(app.get('env') === 'development') {
   app.use(morgan('tiny'));
@@ -35,17 +29,9 @@ app.use(function(req, res, next){
   next();
 })
 
-const courses =[
-  {id: 1, name: 'course1'},
-  {id: 2, name: 'course2'},
-  {id: 3, name: 'course3'}
-]
-
 app.get('/', function(req, res){
   res.render('index', {title: 'My Express APP', message: 'Hello there...'});
 });
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`It's over anakin. I have the ${port} port`));
